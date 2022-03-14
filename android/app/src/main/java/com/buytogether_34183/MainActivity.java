4129cbe82e8dbfbb22763daa2c1e7b6314035bb2
Crowdbotics <team@crowdbotics.com>
2022-03-14 22:54:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/buytogether_34183/MainActivity.java
@@ -0,0 +1,15 @@
+package com.buytogether_34183;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "buytogether_34183";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "buytogether_34183",
+  url: "https://buytogether_34183.botics.co",
+  api: "https://buytogether_34183.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
